aaf8e1c70447572233140848918d8526a4af6a3b
oli-vr-wa <oliver.valido.ramos@gmail.com>
2026-05-07 16:28:14 +0800

    Added a sample project to test the UrlBuilder and updated readme to include information about the sample project.

---
+++ UrlBuilder.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using UrlBuilder.Core;
+
+Console.WriteLine("--- UrlBuilder Sample ---");
+
+// Create an instance of UrlNavigator and use it to build a URLs
+var navigator = new UrlNavigator();
+
+// Build a URL to search an employee by id
+var payrollUrl = navigator
+    .SetController("Financials")
+    .SetAction("Payroll")
+    .AddParameter("employeeId", "99")
+    .Build();
+
+Console.WriteLine($"Payroll URL: {payrollUrl}");
+
+// Build a URL to test special characters in parameters
+var searchUrl = navigator
+    .SetController("Search")
+    .SetAction("Results")
+    .AddParameter("query", "C# URL Builder")
+    .Build();
+
+Console.WriteLine($"Search URL: {searchUrl}");
